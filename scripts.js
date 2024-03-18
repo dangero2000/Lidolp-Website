6ad3715adc113cd0c3b33399161b259d328a66cd
brogar2000 <garrettsworld2000@gmail.com>
2024-03-18 11:56:14 -0400

    Fixed broken directory issues causing About and Videos pages to display a 404 error

--- scripts.js
+++ scripts.js
@@ -1,10 +1,10 @@
 // scripts.js
 document.addEventListener("DOMContentLoaded", function () {
-  fetch('header.html')
+  fetch('/header.html')
     .then(response => response.text())
     .then(header => document.getElementById('header-placeholder').innerHTML = header);
 
-  fetch('footer.html')
+  fetch('/footer.html')
     .then(response => response.text())
     .then(footer => document.getElementById('footer-placeholder').innerHTML = footer);
 });
